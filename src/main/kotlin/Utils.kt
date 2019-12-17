bebe3921c5d3591df1e02862a5eb39c358fc2749
Achraf Amil <achraf.amil@gmail.com>
2019-12-17 19:16:26 +0100

    Solve Day 14

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -5,3 +5,9 @@ fun lcm(x: Long, y: Long, z: Long): Long = lcm(lcm(x, y), z)
 fun lcm(x: Long, y: Long): Long = (x * y) / gcd(x, y)
 
 fun gcd(x: Long, y: Long): Long = if (y != 0L) gcd(y, x % y) else x
+
+fun <K, V> Map<K, V>.mergeReduce(vararg others: Map<K, V>, reduce: (V, V) -> V): Map<K, V> =
+    this.toMutableMap()
+        .apply {
+            others.forEach { other -> other.forEach { merge(it.key, it.value, reduce) } }
+        }
